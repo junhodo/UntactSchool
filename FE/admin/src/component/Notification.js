8c9f88031c3470342b4656f33270011adcb983fc
gilmujjang <alsanrlf@naver.com>
2021-02-25 21:23:49 +0900

    [FIX] 알림에 사용되는 더미데이터의 양식 변경

--- FE/admin/src/component/Notification.js
+++ FE/admin/src/component/Notification.js
@@ -1,119 +1,80 @@
-// import { React, useState } from 'react';
-import React from 'react';
+import { React, useState } from 'react';
 import '../css/Notification.css';
 import { Toast, ToastHeader } from 'reactstrap';
 
 const Notification = () => {
-  // const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState(false);
 
-  //  title  sender  time  content
+  //  title  sender  time  content  id
   const dummy = [
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '1'
     },
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '2'
     },
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '3'
     },
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '4'
     },
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '5'
     },
     {
       title: '정기정검 공지',
       sender: 'GM길무',
       time: '2021-02-24 : 21:15',
       content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
-    },
-    {
-      title: '정기정검 공지',
-      sender: 'GM길무',
-      time: '2021-02-24 : 21:15',
-      content:
-        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.'
+        '안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.안녕하세요 GM길무 입니다. 내일 새볍 04시부터 약 2시간 정도 서버점검이 있을 예정입니다.',
+      id: '6'
     }
   ];
 
   const notificationClick = event => {
     event.preventDefault();
-    console.log(event.target);
-    // setEditing(prev => !prev);
+    setEditing(prev => !prev);
+    const activeNote = event.target.parentNode;
+    console.log(activeNote);
+    if (editing) {
+      console.log({ activeNote });
+    }
   };
 
   const items = dummy.map(item => (
-    <Toast onClick={notificationClick}>
+    <Toast onClick={notificationClick} id={item.id}>
       <ToastHeader>{item.title}</ToastHeader>
       <div className="itemContent">
         <span>{item.sender}</span>
         <span>{item.time}</span>
       </div>
+      <div className="notificationContent">{item.content}</div>
     </Toast>
   ));
 
