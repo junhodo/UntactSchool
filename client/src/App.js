726e124ce6ed28cb52fae52bf16a5062e30ecbf7
jihun1233 <leejihun6535@gmail.com>
2021-02-25 15:26:19 +0900

    [UPDATE] Header.js 내용 추가
    - Navbar 내용 추가. justify-content between으로 정렬.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <p>this is App</p>
+      <Header />
     </div>
   );
 }
--- client/src/component/shared/Header/Header.js
+++ client/src/component/shared/Header/Header.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Nav, Navbar, NavbarBrand } from 'react-bootstrap';
 
 function Header() {
   return (
     <div className="Header">
+      <Navbar bg="dark" expand="lg" className="justify-content-between">
+        <Nav>
+          <Nav.Link className="text-white">Group</Nav.Link>
+        </Nav>
+        <NavbarBrand className="text-white">Clean Study</NavbarBrand>
+        <Nav>
+          <Nav.Link className="text-white">My</Nav.Link>
+        </Nav>
+      </Navbar>
       <p>this is Header</p>
     </div>
   );
