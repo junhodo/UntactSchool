5f1dbc599d555c97c151b3ad03db27bd1f929733
hwanfront <kim015jh@gmail.com>
2021-02-27 21:31:46 +0900

    [Update] sidebar 위치 변경을 위한 수정

--- FE/client/src/App.js
+++ FE/client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './component/shared/Header/Header';
 import MainRouter from './component/shared/MainRouter';
+import Sidebar from './component/shared/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <p>this is App</p>
       <Header />
+      <Sidebar />
       <MainRouter />
     </div>
   );
