cb9d60badb0b6c3005df54b2e7b258cc9b82e053
hwanfront <kim015jh@gmail.com>
2021-02-27 21:30:28 +0900

    [UPDATE] sidebar 작동 확인을 위한 param 추가

--- FE/client/src/component/group/Main/Main.js
+++ FE/client/src/component/group/Main/Main.js
@@ -4,6 +4,15 @@ const Main = () => {
   return (
     <div>
       <p>this is group/main</p>
+      <p>
+        this is group/maithis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainthis is group/mainthis is
+        group/mainthis is group/mainthis is group/mainn
+      </p>
     </div>
   );
 };
