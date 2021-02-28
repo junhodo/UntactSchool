3e0af9dcd7f2228b6258e4d7d545fa502cca375c
hwanfront <kim015jh@gmail.com>
2021-02-28 18:58:24 +0900

    [UPDATE] : 사이드바 프로필 추가
    - 확인을 위해 프로필 클릭 시 기본 프로필이 나오도록 설정함

--- FE/client/src/component/shared/Sidebar/Profile.js
+++ FE/client/src/component/shared/Sidebar/Profile.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Media from 'react-bootstrap/Media';
+import '../../../css/Profile.css';
+import Pepe from '../../../static/image/alone_pepe.jpg';
+import { PersonIcon } from '../../../static/svg/icon';
 
 function Profile() {
+  const [image, setImage] = useState(null);
+
+  const onChangeImage = () => {
+    setImage(Pepe);
+  };
+
+  useEffect(() => {
+    onChangeImage();
+    console.log(image);
+  }, []);
+
   return (
-    <div>
-      <h1>aaaaa</h1>
+    <div className="profile">
+      <Media as="li">
+        <div className="profileWrapper">
+          <button
+            type="button"
+            className="profileIamge"
+            onClick={() => setImage(null)}
+          >
+            {image ? <img src={image} alt="" /> : <PersonIcon />}
+          </button>
+        </div>
+        <div className="profileInfo">
+          <h6>김정환</h6>
+          <p>
+            asdfg32145
+            <br />
+            <small>정보 . . . </small>
+          </p>
+        </div>
+      </Media>
     </div>
   );
 }
