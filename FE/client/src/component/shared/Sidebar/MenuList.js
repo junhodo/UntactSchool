97f6d8451ee53be425652f3020d07186a72b8e84
hwanfront <kim015jh@gmail.com>
2021-02-28 19:02:03 +0900

    [UPDATE] : 사이드바의 프로필과 비교해 목록 스타일 설정

--- FE/client/src/component/shared/Sidebar/MenuList.js
+++ FE/client/src/component/shared/Sidebar/MenuList.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
+import '../../../css/MenuList.css';
 
 function MenuList() {
   return (
     <>
-      <ListGroup defaultActiveKey="#3">
-        <ListGroup.Item action href="#">
-          Link 1
-        </ListGroup.Item>
-        <ListGroup.Item action href="#">
-          Link 2
-        </ListGroup.Item>
-      </ListGroup>
+      <Card>
+        <Card.Header>
+          <Nav variant="tabs" defaultActiveKey="#first">
+            <Nav.Item>
+              <Nav.Link href="#first">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#disabled" disabled>
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional
+            content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </>
   );
 }
