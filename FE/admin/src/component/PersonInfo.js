0440e7f203378666aba432d26e44e1288d382ac4
gilmujjang <alsanrlf@naver.com>
2021-03-03 20:48:26 +0900

    [UPDATE] 회원 상세조회 퍼블리싱 -보기 좋게 만들었다

--- FE/admin/src/component/PersonInfo.js
+++ FE/admin/src/component/PersonInfo.js
@@ -14,7 +14,7 @@ const PersonInfo = () => {
       name: '민무길',
       join: '2021-02-24',
       id: '1',
-      group: ['토익9990방', '정처기방', '투자공부방'],
+      group: ['토익9990방', '정처기방', '투자공부방', '독서방', '한자1급방'],
       reported: ['욕설', '욕설', '도배'],
       age: '24',
       gender: 'male'
@@ -89,33 +89,41 @@ const PersonInfo = () => {
         }
         }`}
       >
-        <div className="userProfile">
-          <img src={userProfile} alt="user profile" />
-          <i className="fas fa-camera fa-2x" />
+        <div className="userInfo">
+          <div className="userProfile">
+            <img src={userProfile} alt="user profile" />
+            <i className="fas fa-camera fa-2x" />
+          </div>
+          <div className="userBox">
+            <div className="userAge">
+              <span>나이 :</span>
+              <span>{item.age}</span>
+            </div>
+            <div className="userGender">
+              <span>성별 :</span>
+              <span>{item.gender}</span>
+            </div>
+            <div className="userJoin">
+              <span>가입정보</span>
+              <span>{item.join}</span>
+            </div>
+          </div>
         </div>
         <div className="userGroup">
-          <span>가입한 그룹</span>
-          <span>
+          <span className="boldText">가입한 그룹</span>
+          <span className="flexWrap">
             {item.group.map(i => (
-              <li>{i}</li>
+              <span className="targetUser">{i}</span>
             ))}
           </span>
         </div>
         <div className="userReported">
-          <span>피신고내역 : </span>
-          <span>
+          <span className="boldText">피신고내역</span>
+          <span className="flexWrap">
             {item.reported.map(i => (
-              <li>{i}</li>
+              <span className="targetUser">{i}</span>
             ))}
           </span>
-        </div>
-        <div className="userAge">
-          <span>나이 : </span>
-          <span>{item.age}</span>
-        </div>
-        <div className="userGender">
-          <span>성별 : </span>
-          <span>{item.gender}</span>
         </div>
       </div>
     </Toast>
