4c5b1b76895bed115e95e887d3eafd13f1ac8f1a
gilmujjang <alsanrlf@naver.com>
2021-03-04 21:55:00 +0900

    [UADATE] 그룹상세정보 정보 추가 -멤버 와 그룹장을 표시했다

--- FE/admin/src/component/GroupInfo.js
+++ FE/admin/src/component/GroupInfo.js
@@ -3,87 +3,177 @@ import { Toast, ToastHeader } from 'reactstrap';
 import '../css/Notification.css';
 
 const GroupInfo = () => {
-  // group  description  people  limit  time   id
+  // group  description  people  limit  time   id  member  manager
   const dummy = [
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '1'
+      id: '1',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '2'
+      id: '2',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '3'
+      id: '3',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '4'
+      id: '4',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '5'
+      id: '5',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '6'
+      id: '6',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '7'
+      id: '7',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '8'
+      id: '8',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '9'
+      id: '9',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     },
     {
       group: '토익합시다',
       description: '@@토익 900점을 위한방@@ 열심히 할사람만 들어오세요!!!',
-      people: '12',
+      people: '6',
       limit: '50',
       time: '2021-02-24 : 21:07',
-      id: '10'
+      id: '10',
+      member: [
+        '민무길',
+        '길무짱',
+        '민길무',
+        '이지훈',
+        '김정환',
+        '타락파워전사'
+      ],
+      manager: '길무짱'
     }
   ];
 
@@ -112,7 +202,19 @@ const GroupInfo = () => {
         }
         }`}
       >
-        {item.description}
+        <div>{item.description}</div>
+        <div className="targetUserList">
+          {item.member.map(i => (
+            <>
+              <i
+                className={`fas fa-crown manager${
+                  i === item.manager ? '' : ' notificationContentHidden'
+                }`}
+              />
+              <span className="targetUser">{i}</span>
+            </>
+          ))}
+        </div>
       </div>
     </Toast>
   ));
